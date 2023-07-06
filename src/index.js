9a304d9260eeb83d05717c8972793c301ffd8a63
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-07-06 11:29:53 +0000

    Add Html files

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ function component() {
     const element = document.createElement('div');
   
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'web'], ' ');
     element.classList.add('hello');
   
     return element;
   }
   
-  document.body.appendChild(component());
+  //document.body.appendChild(component());
