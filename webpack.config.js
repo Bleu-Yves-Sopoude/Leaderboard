478fa7937ce183475d6dad622b780500285ec7ea
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-07-06 10:34:35 +0000

    Add files

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+      title: 'Output Management',
+
+    }),
+
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  devServer: {
+
+    static: './dist',
+
+  },
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
+
+    ],
+
+  },
+};
